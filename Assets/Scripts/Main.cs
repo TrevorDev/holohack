c6dc481d5757c3963063420cbad8cba89b97d1f0
Trevor Baron <trevorjbaron@gmail.com>
2016-07-22 17:25:59 -0700

    holocopter flying

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+    public Vehicle vehicle = null;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //spawn vehicle
+            if(vehicle == null)
+            {
+                vehicle = (Vehicle)Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Vehicle.prefab", typeof(Vehicle)));
+            }
+
+            //place vehicle and reset velocity/position
+            var cam = GameObject.Find("Main Camera");
+            var pos = cam.transform.position + (cam.transform.forward * 15);
+            vehicle.transform.position = pos;
+            vehicle.transform.rotation = new Quaternion();
+            vehicle.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            vehicle.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            
+        }
+	}
+}
