ffa123599678e19bab5d883c1d06f96951fd0c44
Trevor Baron <trbaron@microsoft.com>
2016-08-22 16:33:20 -0700

    add store app

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+        // Update is called once per frame
+    void Update () {
+        if(Input.GetAxis("Jump") > 0)
+        {
+            Vector3 newVel = this.GetComponent<Rigidbody>().velocity;
+            newVel.y = 0.7f;
+            this.GetComponent<Rigidbody>().velocity = newVel;
+        }
+
+        var forwardVel = this.transform.forward * (Input.GetAxis("Vertical") * 1.0f);
+        Vector3 newVel2 = this.GetComponent<Rigidbody>().velocity;
+        newVel2.x = forwardVel.x;
+        newVel2.z = forwardVel.z;
+        this.GetComponent<Rigidbody>().velocity = newVel2;
+
+        this.GetComponent<Rigidbody>().velocity += this.transform.forward * (Input.GetAxis("Vertical") * 0.04f);
+        this.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Horizontal") * 5f, Space.World);
+        //rotate vehicle based on right stick
+        //this.transform.Rotate(new Vector3(Input.GetAxis("RightVertical") * 5f, 0, 0));
+        //this.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("RightHorizontal") * 5f, Space.World);
+        //}
+        //else
+        //{
+        //    this.GetComponent<Rigidbody>().useGravity = false;
+
+        //    this.GetComponent<Rigidbody>().position += this.transform.up * (Input.GetAxis("RightVertical") * 0.05f);
+        //    this.GetComponent<Rigidbody>().position += this.transform.forward * (Input.GetAxis("LeftVertical") * 0.2f);
+        //    this.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("RightHorizontal") * 5f, Space.World);
+        //}
+
+        //if (Input.GetButtonDown("Fire2"))
+        //{
+        //    //var ball = (BouncyBall)Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/BouncyBall.prefab", typeof(BouncyBall)));
+        //    //ball.transform.position = this.GetComponent<Rigidbody>().position + (this.transform.forward * 3);
+        //    //ball.GetComponent<Rigidbody>().velocity = this.transform.forward * 20;
+        //}
+
+        //Previus attempt at rotation (These controls are too hard)
+        //this.GetComponent<Rigidbody>().angularVelocity += new Vector3(0, Input.GetAxis("RightHorizontal")*0.2f, 0);
+        //this.GetComponent<Rigidbody>().angularVelocity += new Vector3(Input.GetAxis("RightVertical") * 0.2f, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+
+public class Main : MonoBehaviour
+{
+    public GameObject helicopterObject;
+    public GameObject bouncyBallObject;
+    public GameObject buildingBlockObject;
+    public Material alt;
+
+    GestureRecognizer gesture;
+
+    bool tapped = false;
+
+    Helicopter helicopter = null;
+
+    int pos = 0;
+    ArrayList blocks = new ArrayList(5);
+    
+    //BouncyBall ball = null;
+    // Use this for initialization
+    void Start()
+    {
+        gesture = new GestureRecognizer();
+        gesture.StartCapturingGestures();
+        gesture.TappedEvent += (a, b, c) =>
+        {
+            tapped = true;
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("n"))
+        {
+            if (helicopter == null)
+            {
+                helicopter = Instantiate(helicopterObject).GetComponent<Helicopter>();
+                //helicopter = (Helicopter)Resources.Load("Assets/Prefabs/Helicopter.prefab");
+                //helicopter = (Helicopter)Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Helicopter.prefab", typeof(Helicopter)));
+            }
+
+            //place vehicle and reset velocity/position
+            var cam = GameObject.Find("Main Camera");
+            var pos = cam.transform.position + (cam.transform.forward * 2);
+            helicopter.transform.position = pos;
+            helicopter.transform.rotation = new Quaternion();
+            helicopter.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            helicopter.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        }
+
+        // Shoot balls
+        if (Input.GetKeyDown("m"))
+        {
+            BouncyBall ball = Instantiate(bouncyBallObject).GetComponent<BouncyBall>();
+
+
+            var cam = GameObject.Find("Main Camera");
+            var pos = cam.transform.position + (cam.transform.forward * 1);
+            ball.transform.position = pos;
+            ball.transform.rotation = new Quaternion();
+            ball.GetComponent<Rigidbody>().velocity = (cam.transform.forward * 6f);
+            ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+
+
+            // Destroy ball after a period of time
+
+            Destroy(ball, 1);
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            var obj = GameObject.Find("spacialmesh");
+            var r = obj.GetComponent<SpatialMappingRenderer>();
+            var temp = r.OcclusionMaterial;
+            r.OcclusionMaterial = alt;
+            alt = temp;
+        }
+
+            // shoot balls from helicopter
+            if (Input.GetKeyDown("v"))
+        {
+            if (helicopter != null)
+            {
+                BouncyBall ball = Instantiate(bouncyBallObject).GetComponent<BouncyBall>();
+
+
+                var pos = helicopter.transform.position + (helicopter.transform.forward * 0.5f);
+                ball.transform.position = pos;
+                ball.transform.rotation = new Quaternion();
+                ball.GetComponent<Rigidbody>().velocity = helicopter.transform.forward * 6f;
+                ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+
+
+                // Destroy ball after a period of time
+
+                Destroy(ball, 1);
+
+            }
+        }
+
+
+        // Place Blocks
+        if (tapped || Input.GetKeyDown("b"))
+        {
+            BuildingBlock block = null;
+            if (blocks.Count < 5) { 
+                block = Instantiate(buildingBlockObject).GetComponent<BuildingBlock>();
+                blocks.Add(block);
+            }else
+            {
+                block = (BuildingBlock)blocks[this.pos];
+                block.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                block.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+                this.pos++;
+                this.pos = this.pos % 5;
+            }
+
+            var cam = GameObject.Find("Main Camera");
+            var pos = cam.transform.position + (cam.transform.forward * 2);
+            block.transform.position = pos;
+            block.transform.rotation = new Quaternion();
+            block.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            block.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+
+            tapped = false;
+        }
+
+        tapped = false;
+    }
+}
+
+
+//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.VR.WSA.Input;
+
+//public class Main : MonoBehaviour
+//{
+//    public GameObject helicopterObject;
+//    GestureRecognizer gesture;
+//    bool tapped = false;
+
+//    Helicopter helicopter = null;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        gesture = new GestureRecognizer();
+//        gesture.StartCapturingGestures();
+//        gesture.TappedEvent += (a, b, c) =>
+//        {
+//            tapped = true;
+//        };
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (tapped || Input.GetButtonDown("Fire1"))
+//        {
+//            if (helicopter == null)
+//            {
+//                helicopter = Instantiate(helicopterObject).GetComponent<Helicopter>();
+//                //helicopter = (Helicopter)Resources.Load("Assets/Prefabs/Helicopter.prefab");
+//                //helicopter = (Helicopter)Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Helicopter.prefab", typeof(Helicopter)));
+//            }
+
+//            //place vehicle and reset velocity/position
+//            var cam = GameObject.Find("Main Camera");
+//            var pos = cam.transform.position + (cam.transform.forward * 2);
+//            helicopter.transform.position = pos;
+//            helicopter.transform.rotation = new Quaternion();
+//            helicopter.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+//            helicopter.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+//        }
+
+//        tapped = false;
+//    }
+//}
